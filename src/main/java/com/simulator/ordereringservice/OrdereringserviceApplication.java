0e3d548d4bdfb85dac471e5044c02ce50f425be2
harsh <harshvardhans57@gmail.com>
2020-03-07 12:02:27 +0530

    Initial Commit

---
+++ src/main/java/com/simulator/ordereringservice/OrdereringserviceApplication.java
@@ -0,0 +1,13 @@
+package com.simulator.ordereringservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdereringserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdereringserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simulator/ordereringservice/OrdereringserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simulator.ordereringservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdereringserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
